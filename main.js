2d7a41e1738592ff1ad18e6dff006d2900f5d9e4
LuckyButterDuck <132005174+LuckyButterDuck@users.noreply.github.com>
2023-06-13 12:09:40 +0300

    update menu, add animated, update header, update logo image

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const menuPhoneNav = document.getElementById('menu-phone-nav')
 const menuContact = document.getElementById('menu-contact')
 const spanMenuNav = document.getElementById('span-menu-nav')
+const menuContactBar = [
+	document.getElementById('menu-contact-bar-one'),
+	document.getElementById('menu-contact-bar-two')
+]
 
 const liMenuNavOne = document.getElementById('li-menu-nav-one')
 const liMenuNavTwo = document.getElementById('li-menu-nav-two')
@@ -10,39 +14,64 @@ let count = 0
 
 menuContact.addEventListener('click', e => {
 	if (count == 0) {
+		menuContact.style.borderRadius = '50px 50px 0 0' // Убрать закругление колобка снизу
+		
+		menuContactBar[0].style.transform = 'rotate(45deg)'
+		menuContactBar[1].style.transform = 'rotate(-45deg)'
+		menuContactBar.forEach(element => {
+			element.classList.remove('menu-contact-bar')
+			element.classList.add('menu-contact-bar-active')
+		})
+		
+
 		setTimeout(() => {
-			menuPhoneNav.style.visibility = 'visible' // Показать меню
-			menuPhoneNav.style.right = '0' // Отодвинуть меню на место
-			menuContact.style.borderRadius = '50px 50px 0 0' // Убрать закругление колобка снизу
-			menuPhoneNav.style.top = '100%' // Поставить меню вниз
-			menuPhoneNav.style.height = '360%' // Выставить высоту меню
-			menuPhoneNav.style.borderRadius = '10px 0 10px 10px' // Закруглить меню
-			spanMenuNav.style.visibility = 'visible' // Показать текст 
+			new Promise((resolve, reject) => {
+				menuPhoneNav.style.visibility = 'visible' // Показать меню
+				menuPhoneNav.style.height = '360%' // Выставить высоту меню
+				menuPhoneNav.style.borderRadius = '0 0 10px 10px' // Закруглить меню
+				setTimeout(() => resolve(), 300)
+			})
+				.then(() => {
+					menuPhoneNav.style.width = '100%' // Выставить ширину меню
+					menuPhoneNav.style.borderRadius = '10px 0 10px 10px' // Закруглить меню
+				})
 			setTimeout(() => {
+				spanMenuNav.style.visibility = 'visible' // Показать текст 
 				liMenuNavOne.style.visibility = 'visible' // Показать текст
 				liMenuNavTwo.style.visibility = 'visible' // Показать текст
 				liMenuNavThree.style.visibility = 'visible' // Показать текст
-				// liMenuNav.style.transition = '0s'
-			}, 300)
+				count = 1
+			}, 400)
+		}, 400)
 
-
-			count = 1
-		}, 300)
 	} else {
-		setTimeout(() => {
+		menuContactBar[0].style.transform = 'rotate(0)'
+		menuContactBar[1].style.transform = 'rotate(0)'
+		menuContactBar.forEach(element => {
+			element.classList.remove('menu-contact-bar-active')
+			element.classList.add('menu-contact-bar')
+		})
+		
+
+		new Promise((resolve) => {
+			menuPhoneNav.style.width = '22.2%'
 			spanMenuNav.style.visibility = 'hidden' // Спрятать текст
 			liMenuNavOne.style.visibility = 'hidden' // Спрятать текст
 			liMenuNavTwo.style.visibility = 'hidden' // Спрятать текст
 			liMenuNavThree.style.visibility = 'hidden' // Спрятать текст
-			menuPhoneNav.style.visibility = 'hidden'
-			menuPhoneNav.style.right = '10%'
+			menuPhoneNav.style.borderRadius = '0 0 10px 10px'
+			setTimeout(() => resolve(), 300)
+		})
+			.then(() => {
+				menuPhoneNav.style.borderRadius = '0 0 50px 50px'
+				menuPhoneNav.style.height = '0'
+				menuPhoneNav.style.visibility = 'hidden'
+			})
+		setTimeout(() => {
 			menuContact.style.borderRadius = '50px'
-			menuPhoneNav.style.top = '0'
-			menuPhoneNav.style.height = '0'
-			menuPhoneNav.style.borderRadius = '0'
-			// liMenuNav.style.transition = '.3s'
-
 			count = 0
-		}, 300)
+		}, 670)
+
+		// }, 300)
 	}
 })
