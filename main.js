91b55f3ddd54691fdbef7524be0a3d86559a6427
LuckyButterDuck <132005174+LuckyButterDuck@users.noreply.github.com>
2023-06-20 09:13:46 +0300

    rewrite animation menu add closing menu when user click on window

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const menuPhoneNav = document.getElementById('menu-phone-nav')
 const menuContact = document.getElementById('menu-contact')
 const spanMenuNav = document.getElementById('span-menu-nav')
+const exitAria = document.getElementById('exit-aria')
+const menuBlock = document.getElementById('menu-block')
+const menuGeneralBlock = document.getElementById('menu-general-block')
 const menuContactBar = [
 	document.getElementById('menu-contact-bar-one'),
 	document.getElementById('menu-contact-bar-two')
@@ -15,23 +18,27 @@ let count = 0
 menuContact.addEventListener('click', e => {
 	if (count == 0) {
 		menuContact.style.borderRadius = '50px 50px 0 0' // Убрать закругление колобка снизу
-		
 		menuContactBar[0].style.transform = 'rotate(45deg)'
 		menuContactBar[1].style.transform = 'rotate(-45deg)'
 		menuContactBar.forEach(element => {
 			element.classList.remove('menu-contact-bar')
 			element.classList.add('menu-contact-bar-active')
 		})
-		
+
 
 		setTimeout(() => {
 			new Promise((resolve, reject) => {
+				menuBlock.style.height = '124px'
 				menuPhoneNav.style.visibility = 'visible' // Показать меню
-				menuPhoneNav.style.height = '360%' // Выставить высоту меню
+				menuPhoneNav.style.height = '100%' // Выставить высоту меню
+				exitAria.style.height = '100vh' // Выставить высоту закрытия
+				exitAria.style.width = '100vw' // Выставить ширину закрытия
+				exitAria.style.zIndex = '4'
 				menuPhoneNav.style.borderRadius = '0 0 10px 10px' // Закруглить меню
 				setTimeout(() => resolve(), 300)
 			})
 				.then(() => {
+					menuBlock.style.width = '134.5px'
 					menuPhoneNav.style.width = '100%' // Выставить ширину меню
 					menuPhoneNav.style.borderRadius = '10px 0 10px 10px' // Закруглить меню
 				})
@@ -51,10 +58,14 @@ menuContact.addEventListener('click', e => {
 			element.classList.remove('menu-contact-bar-active')
 			element.classList.add('menu-contact-bar')
 		})
-		
+
 
 		new Promise((resolve) => {
-			menuPhoneNav.style.width = '22.2%'
+			menuBlock.style.width = '34.5px'
+			menuPhoneNav.style.width = '100%'
+			exitAria.style.width = '0vw'
+			exitAria.style.height = '0vh'
+			exitAria.style.zIndex = '-1'
 			spanMenuNav.style.visibility = 'hidden' // Спрятать текст
 			liMenuNavOne.style.visibility = 'hidden' // Спрятать текст
 			liMenuNavTwo.style.visibility = 'hidden' // Спрятать текст
@@ -65,6 +76,8 @@ menuContact.addEventListener('click', e => {
 			.then(() => {
 				menuPhoneNav.style.borderRadius = '0 0 50px 50px'
 				menuPhoneNav.style.height = '0'
+				exitAria.style.height = '0'
+				menuBlock.style.height = '34.5px'
 				menuPhoneNav.style.visibility = 'hidden'
 			})
 		setTimeout(() => {
@@ -74,4 +87,41 @@ menuContact.addEventListener('click', e => {
 
 		// }, 300)
 	}
+})
+
+exitAria.addEventListener('click', event => {
+	menuContactBar[0].style.transform = 'rotate(0)'
+	menuContactBar[1].style.transform = 'rotate(0)'
+	menuContactBar.forEach(element => {
+		element.classList.remove('menu-contact-bar-active')
+		element.classList.add('menu-contact-bar')
+	})
+
+
+	new Promise((resolve) => {
+		menuBlock.style.width = '34.5px'
+		menuPhoneNav.style.width = '100%'
+		exitAria.style.width = '0vw'
+		exitAria.style.height = '0vh'
+		exitAria.style.zIndex = '-1'
+		spanMenuNav.style.visibility = 'hidden' // Спрятать текст
+		liMenuNavOne.style.visibility = 'hidden' // Спрятать текст
+		liMenuNavTwo.style.visibility = 'hidden' // Спрятать текст
+		liMenuNavThree.style.visibility = 'hidden' // Спрятать текст
+		menuPhoneNav.style.borderRadius = '0 0 10px 10px'
+		setTimeout(() => resolve(), 300)
+	})
+		.then(() => {
+			menuPhoneNav.style.borderRadius = '0 0 50px 50px'
+			menuPhoneNav.style.height = '0'
+			exitAria.style.height = '0'
+			menuBlock.style.height = '34.5px'
+			menuPhoneNav.style.visibility = 'hidden'
+		})
+	setTimeout(() => {
+		menuContact.style.borderRadius = '50px'
+		count = 0
+	}, 670)
+
+	// }, 300)
 })
